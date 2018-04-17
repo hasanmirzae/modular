2f42144234eb428126314eff979d993a30177552
Hassan Mirzaee <hassan_mirzaee@epam.com>
2018-04-17 14:06:32 +0200

    added constructor to Configuration

--- src/main/java/com/github/hasanmirzae/module/Configuration.java
+++ src/main/java/com/github/hasanmirzae/module/Configuration.java
@@ -1,8 +1,30 @@
 package com.github.hasanmirzae.module;
 
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Configuration extends HashMap<String, String> {
+
+    public Configuration(){
+        super();
+    }
+
+    public Configuration(String configSrt){
+        parse(configSrt);
+    }
+
+    private void parse(String configStr) {
+        try{
+            StringTokenizer st = new StringTokenizer(configStr,",");
+            String pair;
+            while (st.hasMoreTokens()) {
+                pair = st.nextToken();
+                put(pair.split(":")[0],pair.split(":")[1]);
+            }
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
 
     public Configuration add(String name, String value){
         put(name,value);
